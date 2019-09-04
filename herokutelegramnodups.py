7aa562ebdcfd781ebcb07d2cad389e98d98f12f4
Michael Pavlov <mr.michael.pavlov@gmail.com>
2019-09-04 13:51:32 +0300

    refactoring

--- herokutelegramnodups.py
+++ herokutelegramnodups.py
@@ -8,45 +8,38 @@ import threading
 class HerokuTgNoDups:
 
     def __init__(self, lifetime_seconds=1):
-        self.sessions = []
+        self.sessions = {}
         self.lifetime_s = lifetime_seconds
 
     def in_cache(self, user_, command_):
         ts_ = datetime.datetime.now()
-        for session in self.sessions:
-            if session["user"] == user_ and session["command"] == command_:
-                # session exists, it means duplicate message received. Need to update timestamp
-                session["ts"] = ts_
-                print("update", session)
-                return True
-
-        # if nothing found need to create new item
-        session = {}
-        session["user"] = user_
-        session["command"] = command_
-        session["ts"] = ts_
-        self.sessions.append(session)
-        print("add", session)
-        return False
+        key = str(user_) + str(command_)
+        if self.sessions.get(key) is not None:
+            # session exists, it means duplicate message received. Need to update timestamp
+            self.sessions[key] = ts_
+            print("update", self.sessions)
+            return True
+        else:
+            # if nothing found need to create new item
+            self.sessions[key] = ts_
+            print("add", self.sessions)
+            return False
 
     def gc(self, ts_):
-        i = 0
-        while i < len(self.sessions) and len(self.sessions) > 0:
-            try:
-                session = self.sessions[i]
-                if session["ts"] + datetime.timedelta(seconds=self.lifetime_s) < ts_:
-                    print("remove", session["ts"], ts_)
-                    self.sessions.remove(session)
-                else:
-                    i += 1
-            except Exception as e:
-                pass
+        try:
+            for key, value in self.sessions.copy().items():
+                if value + datetime.timedelta(seconds=self.lifetime_s) < ts_:
+                    print("remove", key)
+                    self.sessions.pop(key)
+        except Exception as e:
+            print("error:", e)
+            pass
 
     def run_gc(self):
         while True:
             current_ts = datetime.datetime.now()
             self.gc(current_ts)
-            time.sleep(1)
+            time.sleep(.500)
 
     def run(self):
         self.t_gc = threading.Thread(target=self.run_gc,daemon=True)
