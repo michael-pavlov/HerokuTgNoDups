8ee34619638e97ce21b5bb3943c5c9db585370fd
Michael Pavlov <mr.michael.pavlov@gmail.com>
2019-09-04 14:32:17 +0300

    Update herokutelegramnodups.py
    add logging

--- herokutelegramnodups.py
+++ herokutelegramnodups.py
@@ -4,12 +4,26 @@
 import datetime
 import time
 import threading
+import logging
+from logging.handlers import RotatingFileHandler
 
 class HerokuTgNoDups:
 
-    def __init__(self, lifetime_seconds=1):
+    def __init__(self, lifetime_seconds=1, gc_interval_seconds=.500, logger=None):
         self.sessions = {}
         self.lifetime_s = lifetime_seconds
+        self.gc_inteval_seconds = gc_interval_seconds
+
+        if logger is None:
+            self.logger = logging.getLogger("HerokuTgNoDups")
+            self.logger.setLevel(logging.INFO)
+            fh = RotatingFileHandler("herokutgnodups.log", mode='a', encoding='utf-8', backupCount=5,
+                                     maxBytes=1 * 1024 * 1024)
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            fh.setFormatter(formatter)
+            self.logger.addHandler(fh)
+        else:
+            self.logger = logger
 
     def in_cache(self, user_, command_):
         ts_ = datetime.datetime.now()
@@ -17,29 +31,29 @@ class HerokuTgNoDups:
         if self.sessions.get(key) is not None:
             # session exists, it means duplicate message received. Need to update timestamp
             self.sessions[key] = ts_
-            print("update", self.sessions)
+            self.logger.debug("HerokuTgNoDups.in_cache(): update session: "  + key + ", " + str(ts_) + "; Sessions: " + str(self.sessions))
             return True
         else:
             # if nothing found need to create new item
             self.sessions[key] = ts_
-            print("add", self.sessions)
+            self.logger.debug("HerokuTgNoDups.in_cache(): add session: " + key + ", " + str(ts_) + "; Sessions: " + str(self.sessions))
             return False
 
     def gc(self, ts_):
         try:
             for key, value in self.sessions.copy().items():
                 if value + datetime.timedelta(seconds=self.lifetime_s) < ts_:
-                    print("remove", key)
                     self.sessions.pop(key)
+                    self.logger.debug("HerokuTgNoDups.gc(): remove session: " + key)
         except Exception as e:
-            print("error:", e)
+            self.logger.error("HerokuTgNoDups.gc(): " + str(e).replace("\n","")  + "; Sessions: " + str(self.sessions))
             pass
 
     def run_gc(self):
         while True:
             current_ts = datetime.datetime.now()
             self.gc(current_ts)
-            time.sleep(.500)
+            time.sleep(self.gc_inteval_seconds)
 
     def run(self):
         self.t_gc = threading.Thread(target=self.run_gc,daemon=True)
